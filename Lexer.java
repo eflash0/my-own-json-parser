da043a699894959dd68dd93ddc988dc894d6a00f
unknown <edwardarahi2002@gmail.com>
2025-03-09 19:44:39 +0100

    string tokenizing

--- Lexer.java
+++ Lexer.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Lexer{
 
@@ -9,7 +10,57 @@ public class Lexer{
         this.json = json.trim();
     }
 
-    public ArrayList<JSONToken> tokenize(){
-        
+    public List<JSONToken> tokenize(){
+        List<JSONToken> tokens = new ArrayList<>();
+        while(current < json.length()) {
+            char c = json.charAt(current);
+            switch (c) {
+                case '{':
+                    tokens.add(new JSONToken(JSONTokenType.START_OBJECT, "{"));
+                    current++;
+                    break;
+                case '}':
+                    tokens.add(new JSONToken(JSONTokenType.END_OBJECT, "}"));
+                    current++;
+                    break;
+                case '[':
+                    tokens.add(new JSONToken(JSONTokenType.START_ARRAY, "["));
+                    current++;
+                    break;  
+                case ']':
+                    tokens.add(new JSONToken(JSONTokenType.END_ARRAY, "]"));
+                    current++;
+                    break;  
+                case ':':
+                    tokens.add(new JSONToken(JSONTokenType.COLON, ":"));
+                    current++;
+                    break;    
+                case ',':
+                    tokens.add(new JSONToken(JSONTokenType.COMMA, ","));
+                    current++;
+                    break;
+                case '"':
+                    String s = "";
+                    c = json.charAt(++current);
+                    while(c != '"') {
+                        s += c;
+                        c = json.charAt(++current);
+                    }
+                    tokens.add(new JSONToken(JSONTokenType.STRING, s));
+                    current++;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    public String readString(int position){
+        String s = "";
+        char c = json.charAt(++position);
+        while(c != '"'){
+            s += c;
+            c = json.charAt(++position);
+        }
     }
 }
